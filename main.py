648895bea431bbc70eab7c704e5b244367ecb7e4
Fotheringtoon <seb.mccormack@gmail.com>
2026-08-15 21:47:03 +0100

    Added Wedneday While loop

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from meals_list import meals_list
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 random.shuffle(meals_list)
 
-mealPlan = {}
+meal_plan = {}
 
 for day, meal in zip(days, meals_list):
-    mealPlan [day] = meal
-print(mealPlan)
+    meal_plan [day] = meal
+print(meal_plan)
 
 change_wednesday = random.choice(meals_list)
 
-while change_wednesday == mealPlan['Wednesday']:
+while change_wednesday in meal_plan.values():
     change_wednesday = random.choice(meals_list)
 
-mealPlan ['Wednesday'] = change_wednesday
+meal_plan ['Wednesday'] = change_wednesday
