ae7c3e80b28b3f3653fbeae9241ebe90f46563e7
Fotheringtoon <seb.mccormack@gmail.com>
2026-08-15 21:29:04 +0100

    Added change Wednesday button

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from meals_list import meals_list
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 random.shuffle(meals_list)
 
-for day, meal in zip(days, meals_list):
-    print(f"{day}: {meal}" )
+mealPlan = {}
 
+for day, meal in zip(days, meals_list):
+    mealPlan [day] = meal
+print(mealPlan)
+
+change_wednesday = random.choice(meals_list)
+
+while change_wednesday == mealPlan['Wednesday']:
+    change_wednesday = random.choice(meals_list)
+
+mealPlan ['Wednesday'] = change_wednesday
