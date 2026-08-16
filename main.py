fd6259e5a9ec846a26d62f7bc80cd985638f8a02
Fotheringtoon <seb.mccormack@gmail.com>
2026-08-16 10:57:32 +0100

    Added validation to the change day meal function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
 from meals_list import meals_list
 
+def change_meal(day_to_change):
+    change_meal = random.choice(meals_list)
+
+    while change_meal in meal_plan.values():
+        change_meal = random.choice(meals_list)
+
+    meal_plan [day_to_change] = change_meal
+
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 random.shuffle(meals_list)
 
@@ -10,9 +18,19 @@ for day, meal in zip(days, meals_list):
     meal_plan [day] = meal
 print(meal_plan)
 
-change_wednesday = random.choice(meals_list)
 
-while change_wednesday in meal_plan.values():
-    change_wednesday = random.choice(meals_list)
+more_days_to_change = "no"
+day_change_question = input("Would you like to change any of the days?").lower()
+if day_change_question == "yes":
+    more_days_to_change = "yes"
 
-meal_plan ['Wednesday'] = change_wednesday
+while more_days_to_change == "yes":
+    day_to_change = input("What day would you like to change?").title() 
+    if day_to_change in days:
+       change_meal(day_to_change)  
+       more_days_to_change = input("Would you like to change any other days?").lower() 
+    else:
+        print("You haven't entered a valid day!")
+
+  
+print(meal_plan)
